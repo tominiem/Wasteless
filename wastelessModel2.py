c1635870f9479e54849d77a4813ab092943a200e
tominiem <tomi.nieminen@jamk.fi>
2020-10-19 19:53:58 +0300

    Update wastelessModel2.py

--- wastelessModel2.py
+++ wastelessModel2.py
@@ -2,6 +2,8 @@ import pyodbc
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
+import re
+
 #%%
 server = 'wasteless.database.windows.net' 
 database = 'Wasteless' 
@@ -11,12 +13,25 @@ cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';
 cursor = cnxn.cursor()
 
 
-df = pd.read_sql_query('select x.DateSid, ProductionWasteKg, LineWasteKg, PlateWasteKg, WasteTotalKg,MealCount, SpecialMealCount, MealTotal, Menu, 1 LocationSID from dw.dimDate x left join [DW].[FacWasteless] y on x.datesid = y.datesid and y.LocationSID = 1 left join DW.FacMenu z on z.DateSID = x.DateSID and z.LocationSID = 1 where x.Date <= dateadd(day,5,getdate()) and x.Date >= convert(datetime, convert(char(8),20171127)) and x.WorkDay = 1 order by datesid asc',cnxn)    
+df = pd.read_sql_query('select x.DateSID, ProductionWasteKg, LineWasteKg, PlateWasteKg, WasteTotalKg,MealCount, SpecialMealCount, MealTotal, Menu, 1 LocationSID from dw.dimDate x left join [DW].[FacWasteless] y on x.datesid = y.datesid and y.LocationSID = 1 left join DW.FacMenu z on z.DateSID = x.DateSID and z.LocationSID = 1 where x.Date <= dateadd(day,5,getdate()) and x.Date >= convert(datetime, convert(char(8),20171127)) and x.WorkDay = 1 order by datesid asc',cnxn)    
+dfevents = pd.read_sql_query('select DateSID, EventDesc, PackedLunchCount from DW.FacEventCalendar', cnxn)
+dfstudents = pd.read_sql_query('select DateSID, StudentCountTotal from DW.FacStudentCountPerDay', cnxn)
+
 #%%
-df['Date'] = pd.to_datetime(df['DateSid'], format='%Y%m%d')
-today = pd.to_datetime('2020-02-24')
+df['Date'] = pd.to_datetime(df['DateSID'], format='%Y%m%d')
+dfevents['Date'] = pd.to_datetime(dfevents['DateSID'], format='%Y%m%d')
+dfstudents['Date'] = pd.to_datetime(dfstudents['DateSID'], format='%Y%m%d')
+
+today = pd.to_datetime('2020-10-19')
 #%%
-#print(type(df['Menu'][0]))
+dfevents['PackedLunchCount'].fillna(0, inplace=True)
+df['PackedLunchCount'] = df.apply(lambda row: np.max(dfevents['PackedLunchCount'].loc[dfevents['Date']==row['Date']]), axis=1)
+df['PackedLunchCount'].fillna(0, inplace=True)
+df['Event'] = df.apply(lambda row: '-'.join(dfevents['EventDesc'].loc[dfevents['Date']==row['Date']]), axis=1)
+df['TET'] = df.apply(lambda row: 1 if 'TET' in row['Event'] else 0, axis=1)
+df['Leirikoulu'] = df.apply(lambda row: 1 if 'leirikoulu' in row['Event'] else 0, axis=1)
+df['StudentCountTotal'] = df.apply(lambda row: np.max(dfstudents['StudentCountTotal'].loc[dfstudents['Date']==row['Date']]), axis=1)
+df['StudentCountTotal'].fillna(method='ffill', inplace=True)
 
 #%%
 df.dropna(subset=['Menu'], inplace=True)
@@ -30,6 +45,8 @@ menucols = [col for col in df.columns if 'MenuCode' in col]
 for i in range(1,6):
     df['MealTotalLag'+str(i)] = df['MealTotal'].shift(i)
 df['MealTotalMA'] = df.apply(lambda row: np.mean([row['MealTotalLag1'],row['MealTotalLag2'],row['MealTotalLag3'],row['MealTotalLag4'],row['MealTotalLag5']]), axis=1)
+#%%
+df['MealTotalMA'].fillna(method='ffill', inplace=True)
 
 #%%
 dftrain = df.dropna(subset=['MealTotal'])
@@ -38,7 +55,7 @@ dftrain = dftrain[dftrain['MealTotal'] < 600]
 
 #%%
 Xmenu = dftrain[menucols].values
-Xother = dftrain[['MealTotalMA']].values
+Xother = dftrain[['MealTotalMA', 'TET', 'Leirikoulu', 'StudentCountTotal']].values
 X = np.concatenate((Xmenu, Xother), axis=1)
 y = np.array(dftrain['MealTotal'])
 
@@ -48,9 +65,9 @@ dftrain['PredictedMealTotal'] = modelMeal.predict(X)
 #%%
 dftrainwaste = df.dropna(subset=['WasteTotalKg'])
 Xmenu = dftrainwaste[menucols].values
-#Xother = dftrainwaste[['MealTotalMA']].values
-#X = np.concatenate((Xmenu, Xother), axis=1)
-X = Xmenu
+Xother = dftrainwaste[['MealTotalMA', 'TET', 'Leirikoulu', 'StudentCountTotal']].values
+X = np.concatenate((Xmenu, Xother), axis=1)
+#X = Xmenu
 y = np.array(dftrainwaste['WasteTotalKg'])
 
 modelWaste = linear_model.LinearRegression()
@@ -61,10 +78,14 @@ dftrainwaste['PredictedWasteTotalKg'] = modelWaste.predict(X)
 dftest = df.loc[(df['Date'] >= today) & (df['Date'] < today + pd.DateOffset(days=8))]
 dftest['MealTotalMA'].fillna(method='ffill', inplace=True)
 Xtestmenu = dftest[menucols].values
-Xtestother = dftest[['MealTotalMA']].values
+Xtestother = dftest[['MealTotalMA', 'TET', 'Leirikoulu', 'StudentCountTotal']].values
 Xtest = np.concatenate((Xtestmenu, Xtestother), axis=1)
 dftest['PredictedMealTotal'] = modelMeal.predict(Xtest)
-Xtest = Xtestmenu
+#Xtest = Xtestmenu
 dftest['PredictedWasteTotalKg'] = modelWaste.predict(Xtest)
 #%%
+predictmeals = dftest['PredictedMealTotal'].values
+predictwaste = dftest['PredictedWasteTotalKg'].values
 
+print('Predicted meals: ', predictmeals)
+print('Predicted waste: ', predictwaste)
